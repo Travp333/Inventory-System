3f5e910537e120d34e2c66638b8521cf391317c6
Mcdogsman <50517785+Mcdogsman@users.noreply.github.com>
2022-06-26 22:34:52 -0700

    Revert "conflit"
    This reverts commit 8d39c13938e218989b2cdaaf4c851b8302dae3e6.

--- Assets/Scripts/ItemScripts/InventorySlot.cs
+++ Assets/Scripts/ItemScripts/InventorySlot.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [System.Serializable]
-public class InventorySlot
+public class InventorySlot : MonoBehaviour
 {
     [SerializeField] private InventoryItemData itemData;
     [SerializeField] private int stackSize;
