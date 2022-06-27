11381c2ae67e8242613307abc57fad4e35d6830a
Mcdogsman <50517785+Mcdogsman@users.noreply.github.com>
2022-06-26 23:21:12 -0700

    Added ItemPickup

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -11,14 +11,21 @@ public class Interact : MonoBehaviour
     public Transform cam;
     RaycastHit hit;
 
+    public InventoryHolder inventory;
+
+
     // Update is called once per frame
     void Update()
     {
         
         if (Input.GetKeyDown("e")){
             if(Physics.SphereCast(cam.position, 1, cam.forward, out hit, distance, mask)){
-                if(hit.transform.gameObject.GetComponent<Rigidbody>() != null){
-                    Destroy(hit.transform.gameObject);
+                if(hit.transform.gameObject.GetComponent<ItemPickup>() != null){
+                    GameObject g = hit.transform.gameObject;
+                    ItemPickup i = g.GetComponent<ItemPickup>();
+                    inventory.InventorySystem.AddToInventory(i.ItemData, 1);
+                    Destroy(g);
+
                 }
             }
         }
