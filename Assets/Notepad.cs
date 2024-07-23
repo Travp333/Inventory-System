12938e88db80f0d0757c301e56540d4bc695d5e1
Travp333 <Travp333@yahoo.com>
2024-07-23 14:16:08 -0700

    added some notes

---
+++ Assets/Notepad.cs
@@ -0,0 +1,5 @@
+//shift clicking is a bit bugged, will sometimes sent to the wrong inventory
+
+// clean up general logic, take lessons from how obtuse this whole system was during Blight Isle
+//make more usable for other people, clean up temporary stuff, make mroe readable, comment, etc. 
+// basically just prepare for merge with FPS project
